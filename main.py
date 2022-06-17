2e043077088f9b797f947836346f4fb1ac1230e6
Pedro H. C <pedrohcm5@gmail.com>
2022-06-17 11:02:10 -0300

    add: a-star node visualization + highlightning path

--- main.py
+++ main.py
@@ -4,37 +4,43 @@ from time import sleep
 from turtle import update
 from maze import Maze
 from node import Node
+from tkinter import messagebox
+from visualize import DashVisualize
 import numpy as np
 import cv2
 import random
-from tkinter import messagebox
+
 
 def updateImage():
     # Generate description text
     text = searchMethodsName[activeSearchMethodIndex]
     font = cv2.FONT_HERSHEY_SIMPLEX
-    description = np.zeros((int(ImgHeight*0.2), int(ImgHeight*height/width), 3), np.uint8)
+    description = np.zeros(
+        (int(ImgHeight*0.2), int(ImgHeight*height/width), 3), np.uint8)
     description.fill(255)
     textsize = cv2.getTextSize(text, font, 1, 2)[0]
     textX = int((description.shape[1] - textsize[0]) / 2)
     textY = int((description.shape[0] + textsize[1]) / 2)
-    cv2.putText(description, text, (textX, textY), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
+    cv2.putText(description, text, (textX, textY),
+                font, 1, (0, 0, 0), 2, cv2.LINE_AA)
 
     # Resize and show the image
-    opencvImage = cv2.resize(maze.cv2Image, (ImgHeight, int(ImgHeight*height/width)), interpolation = cv2.INTER_AREA)
+    opencvImage = cv2.resize(maze.cv2Image, (ImgHeight, int(
+        ImgHeight*height/width)), interpolation=cv2.INTER_AREA)
     cv2.imshow(title, np.concatenate((opencvImage, description), axis=0))
     cv2.waitKey(1)
+
 
 def randomSearch(maze, delay):
     # Variables
     global nMoves
     path = []
-    
+
     # While the end point isn't reached (or key "s" is pressed)
     while maze.current != maze.endPoint:
         if cv2.waitKey(1) == ord('s'):
             return
-            
+
         # Make a random move
         current = maze.current
         validMoves = maze.GetValidMoves(current[0], current[1])
@@ -45,7 +51,7 @@ def randomSearch(maze, delay):
         nMoves += 1
         updateImage()
         sleep(delay/1000)
-        
+
     # Highlight the path
     maze.highlightPath(path)
 
@@ -56,13 +62,14 @@ def depthSearch(maze, delay):
     global nMoves
     path = []
     backtrack = 0
-    
+
     # While the end point isn't reached
     while maze.current != maze.endPoint:
         # Get every valid move (not visited closed to visited cells)
         current = maze.current
         validMoves = maze.GetValidMoves(current[0], current[1])
-        newMoves = [move for move in validMoves if move not in maze.visitedCells]
+        newMoves = [
+            move for move in validMoves if move not in maze.visitedCells]
         if len(newMoves) > 0:
             backtrack = 0
             move = newMoves[0]
@@ -81,7 +88,8 @@ def depthSearch(maze, delay):
                 nMoves += 1
                 current = maze.visitedCells[-backtrack]
                 validMoves = maze.GetValidMoves(current[0], current[1])
-                newMoves = [move for move in validMoves if move not in maze.visitedCells]
+                newMoves = [
+                    move for move in validMoves if move not in maze.visitedCells]
                 sleep(delay/1000)
             move = newMoves[0]
             maze.UpdateCurrentPoint(move[0], move[1])
@@ -89,23 +97,25 @@ def depthSearch(maze, delay):
             nMoves += 1
             updateImage()
             sleep(delay/1000)
-            
+
     # Highlight the path
     maze.highlightPath(path)
+
 
 def breadthSearch(maze, delay):
     # Variables
     global nMoves
     path = []
-    
+
     # While the end point isn't reached
     while maze.current != maze.endPoint:
         # Get every valid move (not visited closed to visited cells)
         validMoves = []
         for visitedCell in maze.visitedCells:
             validMoves += maze.GetValidMoves(visitedCell[0], visitedCell[1])
-        newMoves = [move for move in validMoves if move not in maze.visitedCells]
-        
+        newMoves = [
+            move for move in validMoves if move not in maze.visitedCells]
+
         # Make every valid move and verify if the end point is reached
         for move in newMoves:
             nMoves += 1
@@ -115,9 +125,10 @@ def breadthSearch(maze, delay):
             if maze.current == maze.endPoint:
                 break
             sleep(delay/1000)
-            
+
     # Highlight the path
     maze.highlightPath(path)
+
 
 def greedySearch(maze, delay):
     # Euclidean distance
@@ -127,21 +138,21 @@ def greedySearch(maze, delay):
     # Variables
     global nMoves
     path = []
-    
+
     # While the end point isn't reached
     while maze.current != maze.endPoint:
         # Get every valid move (not visited closed to visited cells)
         validMoves = []
         for visitedCell in maze.visitedCells:
             validMoves += maze.GetValidMoves(visitedCell[0], visitedCell[1])
-        
+
         # Calculate f(x) for every valid move (f(x) -> h(x))
         f = [h(move) for move in validMoves]
         for i in range(len(f)):
             # Set the f(x) of already visited cells to infinity
             if validMoves[i] in maze.visitedCells:
                 f[i] = 999999999
-        
+
         # Move to the cell with the lowest f(x) and verify if the end point is reached
         move = validMoves[f.index(min(f))]
         maze.UpdateCurrentPoint(move[0], move[1])
@@ -151,18 +162,19 @@ def greedySearch(maze, delay):
         if maze.current == maze.endPoint:
             break
         sleep(delay/1000)
-        
+
     # Highlight the path
     maze.highlightPath(path)
+
 
 def aStarSearch(maze, delay):
     # h(x) --> Euclidean distance
     def h(x):
         return ((x[0]-maze.endPoint[0])**2 + (x[1]-maze.endPoint[1])**2)**0.5
-    
+
     def coordToNode(coord, prev=None):
         return Node(prev, coord)
-    
+
     global nMoves
     openList, closedList = [], []
     startNode = coordToNode(maze.startPoint)
@@ -170,11 +182,11 @@ def aStarSearch(maze, delay):
     closedList.append(startNode)
     for neighbor in maze.GetValidMoves(startNode.position[0], startNode.position[1]):
         openList.append(coordToNode(neighbor, startNode))
-        
+
     while len(openList) > 0:
         if maze.current == endNode.position:
             break
-    
+
         # Calculate openList f(x)
         for node in openList:
             node.g = node.parent.g + 1
@@ -182,7 +194,7 @@ def aStarSearch(maze, delay):
             node.f = node.g + h(node.position)
             if node in closedList:
                 openList.f = '999999999'
-        
+
         # Move to lowest f(x)
         openListFs = [node.f for node in openList]
         nodeToMove = openList[openListFs.index(min(openListFs))]
@@ -190,24 +202,33 @@ def aStarSearch(maze, delay):
         updateImage()
         nMoves += 1
         sleep(delay/1000)
-        
+
         # Move it to the closed list
         closedList.append(nodeToMove)
         openList.remove(nodeToMove)
-        
+
         # Append all valid moves from that node to the open list
         for neighbor in maze.GetValidMoves(nodeToMove.position[0], nodeToMove.position[1]):
             closedListPositions = [node.position for node in closedList]
             openListPositions = [node.position for node in openList]
             if neighbor not in closedListPositions and neighbor not in openListPositions:
                 openList.append(coordToNode(neighbor, nodeToMove))
-        
+
+    # Highlight the path
+    maze.highlightPath([node.position for node in closedList])
+
+    # Visualize the path
+    DashVisualize(openListPositions, closedListPositions, "A* Search")
+
+
 def manualSearch(maze, delay):
     # Controled by the main loop and object
     pass
 
+
 def nothing(x):
     pass
+
 
 def changeActiveSearch(index):
     # Update the search method based on index
@@ -217,6 +238,7 @@ def changeActiveSearch(index):
     activeSearchMethodIndex = index
     activeSearchMethod = searchMethods[activeSearchMethodIndex]
 
+
 # Parameters
 width, height = 10, 10
 moreThanOnePath = True
@@ -224,8 +246,10 @@ title = 'Maze'
 ImgHeight = 300
 
 # Variables
-searchMethods = [manualSearch, randomSearch, depthSearch, breadthSearch, greedySearch, aStarSearch]
-searchMethodsName = ["Manual Search", "Random Seach", "Depth Search", "Breadth Seatch", "Greedy Best First", "A* Search"]
+searchMethods = [manualSearch, randomSearch, depthSearch,
+                 breadthSearch, greedySearch, aStarSearch]
+searchMethodsName = ["Manual Search", "Random Seach",
+                     "Depth Search", "Breadth Seatch", "Greedy Best First", "A* Search"]
 activeSearchMethodIndex = 0
 activeSearchMethod = searchMethods[activeSearchMethodIndex]
 end = False
@@ -235,21 +259,22 @@ nMoves = 0
 maze = Maze()
 maze.GenerateMaze(width, height, moreThanOnePath, 5)
 opencvImage = maze.GetCv2Image()
-opencvImage = cv2.resize(opencvImage, (ImgHeight, int(ImgHeight*height/width)), interpolation = cv2.INTER_AREA)
+opencvImage = cv2.resize(opencvImage, (ImgHeight, int(
+    ImgHeight*height/width)), interpolation=cv2.INTER_AREA)
 cv2.namedWindow(title)
-cv2.createTrackbar('delay',title , 0, 1000, nothing)
+cv2.createTrackbar('delay', title, 0, 1000, nothing)
 updateImage()
 
 # Main loop
 while True:
     # Get Key Pressed
     keyPressed = cv2.waitKey(1)
-    
+
     # 'q' --> Quit
     if keyPressed == ord('q'):
         cv2.destroyAllWindows
         break
-    
+
     # 'g' --> Generate new maze
     if keyPressed == ord('g'):
         maze.GenerateMaze(width, height, moreThanOnePath)
@@ -273,7 +298,7 @@ while True:
             maze.Restart()
             end = False
             updateImage()
-            
+
         # Activate the selected search method
         try:
             activeSearchMethod(maze, cv2.getTrackbarPos('delay', title))
@@ -311,4 +336,5 @@ while True:
         if maze.current == maze.endPoint:
             end = True
             updateImage()
-            messagebox.showinfo("End", f"Finished in {nMoves} moves with {searchMethodsName[activeSearchMethodIndex]}")
+            messagebox.showinfo(
+                "End", f"Finished in {nMoves} moves with {searchMethodsName[activeSearchMethodIndex]}")
--- visualize.py
+++ visualize.py
@@ -1,25 +1,37 @@
 from dash import Dash, html
 import dash_cytoscape as cyto
 
+
 class DashVisualize:
-    
-    def __init__(self, elements, title):
+
+    def __init__(self, open_elements, closed_elements, title):
+        valid_elements = []
+
+        # Create a list of valid elements
+        for i in range(len(closed_elements)):
+            valid_elements.append(
+                {"data":
+                    {"id": i, "label": f"Node {i}"},
+                 "position":
+                    {"x": closed_elements[i][0]*100,
+                        "y": closed_elements[i][1]*100}
+                 }
+            )
+
+        # Point source -> target on valid_elements
+        for i in range(len(closed_elements)-1):
+            valid_elements.append({"data": {"source": i, "target": i+1}})
+
+        print(valid_elements)
+
         app = Dash(__name__)
-        app.layout = html.Div([
-            html.P(title),
+        app.layout = html.Div(children=[
             cyto.Cytoscape(
                 id=title,
-                elements = elements,
-                # elements=[
-                #     {'data': {'id': 'ca', 'label': 'Canada'}}, 
-                #     {'data': {'id': 'on', 'label': 'Ontario'}}, 
-                #     {'data': {'id': 'qc', 'label': 'Quebec'}},
-                #     {'data': {'source': 'ca', 'target': 'on'}}, 
-                #     {'data': {'source': 'ca', 'target': 'qc'}}
-                # ],
-                layout={'name': 'breadthfirst'},
-                style={'width': '1280px', 'height': '720px'}
-            )
+                elements=valid_elements,
+                style={'width': '1280px', 'height': '720px'},
+                layout={'name': 'preset'}
+            ), f"Open List: {open_elements}\n\nClosed List: {closed_elements}"
         ])
 
-        app.run_server(debug=True)
+        app.run_server()
